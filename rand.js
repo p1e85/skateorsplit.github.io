30d5441c467b4f25bd447e67690c581505798f26
p1creations <47378008+p1creations@users.noreply.github.com>
2022-03-21 14:32:50 -0500

    Delete rand.js

--- rand.js
+++
@@ -1,8 +0,0 @@
-var daddyFeat = '<iframe src="https://player.twitch.tv/?video=v1108705814&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-
-function myFunction() {
-  document.getElementById("featuredContent").innerHTML = daddyFeat;
-}
-
-
-
