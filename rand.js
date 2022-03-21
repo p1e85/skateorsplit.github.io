1a049999905b557f18692a1b9da2d31f39859576
p1creations <47378008+p1creations@users.noreply.github.com>
2022-03-21 14:32:23 -0500

    Add files via upload

---
+++ rand.js
@@ -0,0 +1,8 @@
+var daddyFeat = '<iframe src="https://player.twitch.tv/?video=v1108705814&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+
+function myFunction() {
+  document.getElementById("featuredContent").innerHTML = daddyFeat;
+}
+
+
+
